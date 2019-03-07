c6a4e52c5b8f76ec28db28b57b8099942ced8de5
Bernardo Garza <bernardo.g.landa@gmail.com>
2019-03-07 10:56:34 -0600

    Finished the project

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,65 @@
-function random_item(computerPlay){
-  
-    return computerPlay[Math.floor(Math.random()*computerPlay.length)];
+function playerSelection(){
+    let selection = prompt('Rock, Paper or Scissors?').toLowerCase();
+    return selection;
 }
 
 
-    playerSelection = prompt('Rock, Paper or Scissors?');
-    playerSelection = playerSelection.toLowerCase();
-    console.log('Player: '+ playerSelection);
+function computerSelection(){
+    let ar = ['rock','paper','scissors'];
+    let computerChoice = ar[Math.floor(Math.random()*ar.length)];
+    return computerChoice;
+}
 
-    const computerPlay = ['rock','paper','scissors'];
-    console.log('Computer: ' + random_item(computerPlay));
-    let computerSelection = computerPlay;
+function game(){
+    if (pS == 'rock') {
+        switch (pS=='rock'){
+            case dS == 'rock':
+                console.log("Tie");
+                break;
+            case dS == 'paper':
+                console.log('You lose');
+                break;
+            case dS == 'scissors':
+                console.log('You win');
+                break;
+        }
 
-    if (playerSelection === computerSelection){
-        console.log('tie')
     }
+    else if (pS == 'paper'){
+        switch (pS == 'paper'){
+            case dS == 'rock':
+                console.log('You win');
+                break;
+            case dS == 'paper':
+                console.log('Tie');
+                break;
+            case dS == 'scissors':
+                console.log('You lose');
+                break;
+        }
+    }
+    else if (pS == 'scissors'){
+        switch(pS == 'scissors'){
+            case dS == 'rock':
+                console.log('You win');
+                break;
+            case dS == 'paper':
+                console.log('You lose');
+                break;
+            case dS == 'scissors':
+                console.log('Tie');
+                break;
+        }
+    }
+    else {
+        alert('Invalid input: '+pS);
+    }
+}
+
+let pS = playerSelection();
+console.log ('User: '+pS);
+let dS = computerSelection();
+console.log('Computer: '+dS);
+let result = game();
+
+//por que se declararon las variables despues de los if/switch?
